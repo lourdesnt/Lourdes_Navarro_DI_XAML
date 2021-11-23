1e4b13919830419f4cea04ed0511ed99dbc52025
lourdes <lourdes_navarrotocon@nervion.salesianas.org>
2021-11-23 20:04:12 +0100

    comienzo proyecto, creacion clase piscina y control de piscinas

---
+++ controlador/ControlPiscinas.cs
@@ -0,0 +1,34 @@
+﻿using Lourdes_Navarro_DI_XAML.modelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Lourdes_Navarro_DI_XAML.controlador
+{
+    public class ControlPiscinas
+    {
+        public ObservableCollection<Piscina> ListaPiscinas { get; set; }
+
+        public ControlPiscinas()
+        {
+            ListaPiscinas = new ObservableCollection<Piscina>();
+            ListaPiscinas.Add(new Piscina(Sala.Crustaceos, "20ºC", new string[] { "Cangrejo", "Langosta", "Percebe" }, new DateTime(2021, 11, 23), "Desinfección de la piscina"));
+        }
+
+        public void Agregar(Piscina piscina)
+        {
+            ListaPiscinas.Add(piscina);
+        }
+
+        public void Modificar(int index, Piscina piscina)
+        {
+            ListaPiscinas[index] = piscina;
+        }
+
+        public void Eliminar(int index)
+        {
+            ListaPiscinas.RemoveAt(index);
+        }
+    }
+}
---
+++ modelo/Piscina.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Lourdes_Navarro_DI_XAML.modelo
+{
+    public enum Sala { Tropical, Crustaceos, Mamiferos };
+    public class Piscina : INotifyPropertyChanged
+    {
+        public Sala sala;
+        public Sala Sala
+        {
+            get { return sala; }
+            set
+            {
+                this.sala = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Sala"));
+            }
+        }
+
+        private string temperatura;
+        public string Temperatura
+        {
+            get { return temperatura; }
+            set
+            {
+                this.temperatura = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Temperatura"));
+            }
+        }
+
+        private string[] especies;
+        public string[] Especies
+        {
+            get { return especies; }
+            set
+            {
+                this.especies = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Especies"));
+            }
+        }
+
+        private DateTime ultimaRevision;
+        public DateTime UltimaRevision
+        {
+            get { return ultimaRevision; }
+            set
+            {
+                this.ultimaRevision = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("UltimaRevision"));
+            }
+        }
+
+        private string tratamiento;
+        public string Tratamiento
+        {
+            get { return tratamiento; }
+            set
+            {
+                this.tratamiento = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Tratamiento"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Piscina()
+        {
+
+        }
+
+        public Piscina(Sala sala, string temperatura, string[] especies, DateTime ultimaRevision, string tratamiento)
+        {
+            this.sala = sala;
+            this.temperatura = temperatura;
+            this.especies = especies;
+            this.ultimaRevision = ultimaRevision;
+            this.tratamiento = tratamiento;
+        }
+
+    }
+}
